13161e1e5c2589823758af1df050f2664784eb17
abaker9 <abaker9@ford.com>
2018-04-22 10:09:33 -0400

    modify tasks

---
+++ tutorials/basic/task-outputs-to-inputs/create_some_cheese.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+mkdir some-cheese
+echo "file1" > some-cheese/cheddar
+echo "file2" > some-cheese/colby
+echo "file3" > some-cheese/swiss
+echo "file4" > some-cheese/gouda
+
+ls some-cheese/*
